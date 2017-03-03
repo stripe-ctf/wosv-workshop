da13dfe3924bf618aabad331e2cc2d980c3275e2
Amber Feng <amber@stripe.com>
2017-03-02 16:43:54 -0800

    Un-coffee browser.js in level 6.

--- levels/6/browser.js
+++ levels/6/browser.js
@@ -1,78 +1,81 @@
-# Install instructions:
-#   - Install phantomjs 1.5+
-#     - wget http://phantomjs.googlecode.com/files/phantomjs-1.6.1-linux-x86_64-dynamic.tar.bz2
-#     - tar -xjvf phantomjs-1.6.1-linux-x86_64-dynamic.tar.bz2
-#     - ln -s phantomjs-1.6.1-linux-x86_64-dynamic/bin/phantomjs /usr/bin/local
-#   - Install casperjs
-#     - git clone git://github.com/n1k0/casperjs.git
-#     - cd casperjs
-#     - git checkout tags/1.0.0-RC1
-#     - ln -sf `pwd`/bin/casperjs /usr/local/bin/casperjs
-#   - Run it!
-#     - casperjs browser.coffee http://level04.stripe-ctf.com/
-#     - (change above path to match the actual CTF path)
-#
-# Will exit(0) if success, exit(other) if failure
-# Profit!
+// Install instructions:
+//  - Install phantomjs 1.5+
+//    - brew install phantomjs (or similar)
+//    - http://phantomjs.org/download.html
+//   - Install casperjs
+//     - http://docs.casperjs.org/en/latest/installation.html
+//   - Run it!
+//     - casperjs browser.js http://localhost:4567
+//
+// Will exit(0) if success, exit(other) if failure
+// Profit!
 
-casper = require('casper').create()
-system = require 'system'
-utils = require 'utils'
-fs = require 'fs'
+var casper = require('casper').create();
+var system = require('system');
+var utils = require('utils');
+var fs = require('fs');
 
+if (!casper.cli.has(0)) {
+  console.log('Usage: browser.js <url to visit>');
+  casper.exit(1);
+}
 
-if not casper.cli.has(0)
-    console.log 'Usage: browser.coffee <url to visit>'
-    casper.exit 1
+var password = fs.open('password.txt', 'r').read().trim();
 
-password = fs.open('public_html/password.txt', 'r').read().trim()
+var page_address = casper.cli.get(0);
+console.log('Page address is: ' + page_address);
 
-page_address = casper.cli.get(0)
-console.log "Page address is: #{page_address}"
+casper.start(page_address, function() {
+  // Fill and submit the login form
+  console.log('Filling the login form...');
+  this.fill('form', {username: 'karma_fountain', password: password}, true);
+});
 
-casper.start page_address, ->
-  # Fill and submit the login form
-  console.log 'Filling the login form...'
-  @fill "form", {username: 'level07-password-holder', password: password}, true
+casper.then(function() {
+  // Log the page title.
+  console.log('On the main page')
 
-casper.then ->
-  # Log the page title.
-  console.log "On the main page"
+  var page_title = this.getTitle();
+  var page_url = this.getCurrentUrl();
+  console.log('Before posting title is: ' + page_title + '(url: ' + page_url + ')');
 
-  page_title = @getTitle()
-  page_url = @getCurrentUrl()
-  console.log "Before posting title is: #{page_title} (url: #{page_url})"
+  // TODO: maybe make a post sometimes?
+  var h3 = this.evaluate(function() {
+    return document.querySelectorAll('h3')[0].innerHTML;
+  });
 
-  # TODO: maybe make a post sometimes?
-  h3 = @evaluate ->
-    return document.querySelectorAll('h3')[0].innerHTML
-
-  console.log "First h3 has contents #{h3}"
+  console.log('First h3 has contents ' + h3);
 
   titles = ['Important update', 'Very important update', 'An update', 'An FYI',
             'FYI', 'Did you know...', 'Possibly of interest', 'Definitely of interest',
             "You probably don't care but...", 'Because I feel like posting', 'Note',
-            "You probably don't know", 'Guess what?', 'Might want to take note', 'A really cool update']
+            "You probably don't know", 'Guess what?', 'Might want to take note', 'A really cool update'];
+
   bodies = ['I am hungry', 'Anyone want to play tennis?', 'Up for some racquetball?',
             'Hey!', "I'm bored. Anyone want to play a game?", 'Ooh, I think I found something',
             'Why is it so hard to find good juice restaurants?', 'You should all invite your friends to join Streamer!',
             'Why is everyone trying to exploit Streamer?', 'Streamer is *soo* secure',
-            'Welcome!', 'Glad to have you here!', "I know what you're doing right now. You are reading this message."]
+            'Welcome!', 'Glad to have you here!', "I know what you're doing right now. You are reading this message."];
 
-  # Post infrequently
-  if Math.random() < 0.05
-    console.log "Decided to post"
-    title = titles[Math.floor(Math.random() * titles.length)]
-    body = bodies[Math.floor(Math.random() * bodies.length)]
-    @fill "form#new_post", {title: title, body: body}, true
-  else
-    console.log "Decided not to post"
+  // Post infrequently
+  if (Math.random() < 0.05) {
+    console.log('Decided to post');
+    title = titles[Math.floor(Math.random() * titles.length)];
+    body = bodies[Math.floor(Math.random() * bodies.length)];
+    this.fill('form#new_post', {title: title, body: body}, true);
+  } else {
+    console.log('Decided not to post');
+  }
+});
 
-casper.then ->
-  page_title = @getTitle()
-  page_url = @getCurrentUrl()
-  console.log "After posting title is: #{page_title} (url: #{page_url})"
+casper.then(function() {
+  var page_title = this.getTitle();
+  var page_url = this.getCurrentUrl();
+  console.log('Before posting title is: ' + page_title + '(url: ' + page_url + ')');
+});
 
-casper.run ->
-  console.log "Running!"
-  casper.exit 0
+casper.run(function() {
+  console.log('Running!');
+  casper.exit(0);
+});
+
